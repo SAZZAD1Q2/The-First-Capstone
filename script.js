7a90f8cd210712963c86220a78fb61080a680695
SAZZAD1Q2 <kurulia115@gmail.com>
2023-03-23 21:53:10 +0600

    html file edited

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const toggle = document.querySelector('.toggle');
 const navLink = document.querySelector('.nav-mobile-links');
 
-
 toggle.addEventListener('click', () => {
- navLink.classList.toggle('active');
-})
-
+  navLink.classList.toggle('active');
+});
